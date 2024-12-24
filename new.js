0d76ea82183af1f5efb178cc33a05520248ee5f8
BharathS2202 <bharathsivaraj022@gmail.com>
2024-12-24 13:27:58 +0530

    new push

---
+++ new.js
@@ -0,0 +1,349 @@
+
+// // // //console
+
+
+// // // var sugarbox = "sugar"
+
+// // // console.log(sugarbox)
+
+
+// // // //data types 
+
+// // // //primitive
+
+// // // //non primitive
+
+
+
+// // // // var
+
+// // // // let
+
+// // // // const
+
+// // // // what is variable
+
+
+// // // var a = 10;   //redeclare
+// // //     a = 5;     // reassign
+// // // console.log(a);
+
+
+// // // // var data type is support redeclare values and resign values
+
+
+// // // // let variables
+
+
+// // // // let a = 5;  //it's cannot redeclared
+// // // //     a = 10;  // it's support reassign
+// // // // console.log(a);
+
+
+// // // // let data type is not support redeclared values
+// // // // let data type is support reassign values
+
+
+// // // // const a = 10;  // it's cannot redeclared
+// // // //       a = 10;  // it's cannot reassign
+// // // // console.log(a);
+
+
+// // //  //const data type not support redeclard and reassign values
+
+
+// // //  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+
+
+// // //                                      //Non primitive
+
+
+// // //  //array  - [] - collection of values and datas within the square brackerts
+
+
+// // //  const arr = ["hello",10,10.9,false,null]
+// // //  console.log(arr[3]);
+
+
+// // //  //length = starts with 1
+// // //  //index = starts with 0
+// // //  //length-1 = index
+
+ 
+
+// // //                                 //what is object
+
+// // //                                 //object-{} collection of key and value pairs
+
+// // //    const obj ={
+// // //     name:"sharvesh",
+// // //     age:25,
+// // //     std:x11
+// // //    }
+// // //    console.log(obj);
+   
+
+// // //    //syntax
+
+// // //   //  key : values;
+
+// // // //  const obj = {
+
+// // // //     //name:value
+
+// // // // id : 156,
+// // // // name: "sharvesh",
+// // // // age : 25
+
+// // // // }
+
+// // // // obj()
+
+
+// // // //what is function
+
+// // // //function : block of code
+
+// // // //function var_name(){
+// // // //code
+
+// // // //}
+
+// // // //var_name()
+
+
+// // // // function house(){
+// // // //     var boy = "birth"
+    
+// // // // }
+// // // // house()
+
+
+// // // // parameter & arguments :
+
+// // // // parameter- is like an variable_name
+
+// // // //arguments- is like an value
+
+
+// // // function box(x){
+// // //     console.log(x);
+    
+// // // }
+
+// // // box(10) // arguments<=> value
+
+
+
+// // // var a = 100
+// // // console.log(a);
+
+
+// // // //return statement
+
+// // // function box(x) {
+// // //     return x
+// // // }
+// // // console.log(box("hello js"))
+
+
+
+// // // // function expressions
+
+// // // var bigbox = function smallbox(){
+// // //     console.log("Teddy bear here");
+    
+// // // }
+
+// // // bigbox()
+
+
+// // // function test(x,y){
+// // //     console.log(" hello " + x + " how are you " + y);
+    
+// // // }
+// // // test("jai", "shrish")
+
+
+
+
+// // // function test (x,y,z){
+
+// // //     console.log(" Hi " +  x + " how are you " + y  +  " welcome "  + z   );
+    
+// // // }
+// // // test("sharvesh", "raju", "seshan")
+
+
+
+// // // //imediate invoke function expressions
+
+// // // // it is global level
+// // // // it is support only before functions name is not there its working
+// // // //othervise you store the variable name in function
+
+// // // (function(x){
+// // //     console.log(x);
+    
+// // // }
+// // // ) ("java")
+
+
+// // // // var name = (function(x)){
+// // // console.log(x);
+
+// // // //}("java")
+
+// // // //arrow function : short hand system:
+
+// // // //syntax:
+// // // //()=> {}
+
+// // //     var arrow =(x) => {console.log(x)}
+
+// // //     arrow("happy")
+
+
+// // //     //operators:
+// // //     //Arithmetic operator  (add,sub,mull,div,modules,exponent(power value),increment,decrement)
+// // //     //Assignment operator
+// // //     //Logical operator
+// // //     //Equality operator
+// // //     //comparsion operator
+// // //     //ternary operator
+
+    
+
+// // //     //Arithmetic operator (addition)  (it is create two vari)
+
+// // //     var num = 2
+// // //     var num1 =10
+// // //     console.log(num+num1);
+
+
+
+// // //     var num = 2
+// // //     var num1=5
+// // //     console.log(num*num); // 2*5 // 2*2*2*2*2
+
+
+
+// // //     //Assignment operator  (mulitiple values stored in one variable)
+
+// // //     var num = 10
+// // //     num += 50
+// // //     console.log(num);
+    
+
+
+// // //     //Logical operator:
+
+// // //     //&& =>And
+// // //     // || => or
+// // //     // ! => not
+    
+
+// // //     var num = 10
+// // //     var num2 = 10
+// // //     console.log(num !=num2);
+
+
+// // //     //Equality operator [interview questions]
+// // //     //double equals[loose equality]
+// // //     // it only check the values not the data type
+
+// // //     var de = 10
+// // //     var de2 = "10"
+// // //     console.log(de==de2);
+
+
+// // //     //triple equals === (strictly equality)
+// // //     // it wil check both the values and data types)
+
+// // //     var de = 10
+// // //     var de2 = "10"
+// // //     console.log(de===de2);
+
+
+// // //     //Compression operator
+
+// // //     var de = 10
+// // //     var de2 = 10
+// // //     console.log(de<de2);
+
+// // //     //Ternary operator
+
+// // //     //syntax :
+
+// // //     //(condition) ? "statement_true" : "statement_false"
+
+
+// // // var age = 25
+// // // var limit = (age>18) ? "he is eligible" : "he is not eligible"
+
+// // // console.log(limit);
+
+// // //LOOP: (4th class)
+// // // 1.For loop
+
+// // //Syntax:
+// // //for(initialization ; conditio ; iteration[increment==, decrement --]){
+// // //code
+// // //}
+
+// // // for(var i=0 ; i<5 ; i++){
+// // // console.log(i);
+// // //}
+
+// // //steps:
+
+// // //step1 => i = 0 => 0<5[trure]=> print = 0 =>0++ = 0+1 =1 
+// // //step2 => i = 1 => 1<5[trure]=> print = 1 =>1++ = 1+1 =2
+// // //step3 => i = 2 => 2<5[trure]=> print = 2 =>2++ = 2+1 =3
+// // //step4 => i = 3 => 3<5[trure]=> print = 3 =>3++ = 3+1 =4
+// // //step5 => i = 4 => 4<5[trure]=> print = 4 =>4++ = 4+1 =5
+// // //step6 => i = 5 => 5<5[false] // loop will stop
+
+//string :
+
+// var str = "happy"
+
+// for(var i=0 ; i<str.length ; i++){
+//     console.log(str[i]);  
+//  }
+ 
+//  var str = "happy";
+
+// for (var i = str.length - 1; i >=0; i--) {
+//     console.log(str[i]);
+// }
+
+// // // 2.While loop
+// // // 3.Do while loop
+
+// //let name = "bharath"
+// //let age = 56;
+// //console.log(`hello ${name}`); // template literal
+
+// //While loop
+
+// //Syntax :
+// //While (condition) {
+// //}
+
+// // var num = 1;
+// // while (num <= 5){
+// //     console.log(num);
+// //     num++
+// // }
+
+// // dowhile syntax :
+// let d = 1;
+// do {
+//     console.log(d);
+//     d++
+    
+// } while (d<=5);
+
+
---
+++ script.js
@@ -0,0 +1,257 @@
+
+
+
+
+//How to write js code :
+
+//Variable => container to store values
+
+///Syntax : [Formulae]
+//variable_type variable_name = value
+//console.log(variable_name);
+
+//Variable types :
+
+//var
+
+// var a = 10 // var can both redeclare and reassifn
+// var a = 5
+// console.log(a);
+
+// var a = 10  // var can reassign
+//     a = 5
+// console.log(a);
+
+//let => let can only reassign, it does not support redeclare
+
+//const => const does not support both redeclare and reassign
+
+//Datatypes :
+
+//Primitive datatype :
+
+//string - " " or ' '
+// var a = "bharath";
+//console.log(a);
+
+//number - 1234 or 21.4
+// var a = 10;
+//console.log(a);
+
+//boolean - true or false
+// var a = 10
+// var b = 5
+// console.log(a>b);
+
+//null
+// var a = null;
+//console.log(a);
+
+//undefined
+//var a
+//console.log(a);
+
+//Non primitive dadatypes :
+
+// //array : [] => collection of multiple values and datas inside square bracket, it can store multiple datatypes....
+// var a = ["hello", 5, null, true ]
+// console.log(a);
+
+// To print a particular value or data inside array we will use index value inside printing statement :
+// var a = ["hello", 5, null, true ]
+// console.log(a[3]); => true will be printed
+
+// length starts with 1(1,2,3,4....)
+// index value starts with 0(0,1,2,3....)
+
+//object : {} => collection of key and value pairs inside flower bracket
+
+// var ID = {
+//     // key : value
+//     name : "Bharath",
+//     age : 24,
+//     Dep : "B.sc" 
+// } 
+// console.log(ID);
+
+
+//Function : block of code => we are going to write set of code and store it in function and we will use it when it is required
+
+//function statement or declaration
+
+//syntax :
+
+//function var_name(){
+// code
+//}
+//var_name()
+
+// // function box(){
+// console.log("toys");
+// console.log("dresses");
+// }
+// box()
+
+// parameter and arguement is used only inside function
+//parameter - it acts like a variable_name
+//arguement - it acts like a value
+
+// function box2(x){ => parameter ( parameter should be given in paranthesis near variable name in function)
+//     console.log(x);
+// }
+// box2(100) => arguement( is value given when the variable name is called outside function)
+
+// return :
+
+//  function box3(y){
+//     return y
+//  }
+// console.log(box3("choclate"));
+
+//function expression : storing a function under a variable name
+
+//  var bigbox = function smallbox(){
+//     console.log("toys"); 
+//  }
+//  bigbox()
+
+    //var bigbox = function(){  => anonymous funcation (no variable name near function in function expression)
+    //     console.log("toys"); 
+    //  }
+    //  bigbox()
+
+//immediate invoke function expression [IIFE] : will not have variable name , we will use only parameter and value should be given or called 
+ //                                                near flower bracket
+
+//  var nam = (function(x){               
+//     console.log(x);
+// })(10)
+
+//arrow function : short hand syntax
+
+//syntax :
+// () =>{ }
+
+//   var arr = ()=>{console.log("iam arrow function"); }
+//   arr()
+
+//operators :
+
+//Arithmatic operators
+//+add, -sub, *mul, /div, %modulus[will give quotiest value], **exponent[will give remainder value], ++increment , --decrement
+
+//Assignment 
+
+// var an = 10
+// an= 5
+// console.log(an);
+
+//Logical 
+
+// && => and
+// || => or
+// != => not
+
+//comparision
+// < less tha , > greater than , <= less than or equal , >= greater than or equal
+
+//Eqality
+
+// double equals ==   => only checks and compares value , will not check datatype
+// triple equals ===  => checks and compares values and datatypes..Will print true only when the datatypes are true
+
+// var a = 10
+// var b = "10"
+// console.log(a==b);      => true
+
+//  var a = "10"
+//  var b = "10"
+//  console.log(a===b);     => true
+
+//Ternary operators :
+
+// syntax :
+
+//(condition) ? "statement_true" : "statement_false"
+
+// var age = 20
+// var limit = (age>=18 && age<=70) ? "he is eligible to vote " : "he is not eligible to vote"
+// console.log(limit);
+
+//Loop : program will continously excute untill the condition is met or executed
+
+//conditional loop
+//Infinity loop
+
+// For loop :
+
+//syntax :
+
+// for(initialization ; condition ; iteration [increment ++, decrement --]){
+             // code
+//}
+
+// for(var i = 0 ; i<=5 ; i++ ){
+//     console.log(i);   
+// }
+
+// for(var i=5 ; i>0 ; i--){
+//  console.log(i);
+//  }
+
+ //steps:
+
+ //step1 => i = 0 => 0<5[trure]=> print = 0 =>0++ = 0+1 =1 
+ //step2 => i = 1 => 1<5[trure]=> print = 1 =>1++ = 1+1 =2
+ //step3 => i = 2 => 2<5[trure]=> print = 2 =>2++ = 2+1 =3
+ //step4 => i = 3 => 3<5[trure]=> print = 3 =>3++ = 3+1 =4
+ //step5 => i = 4 => 4<5[trure]=> print = 4 =>4++ = 4+1 =5
+ //step6 => i = 5 => 5<5[false] // loop will stop
+
+ //string :
+
+// var str = "happy"
+
+// for(var i=0 ; i<str.length ; i++){
+//     console.log(str[i]);  
+//  }
+
+//  var str = "happy"
+//  for(var i = str.length -1 ; i>=0 ; i-- ){   => to print happy in reverse(yppah)
+//     console.log(str[i]);
+//  }
+
+// var str = "happy";
+// var store = "";               // Temporary variable to store the store
+// for (var i = str.length-1; i >= 0; i--) {
+//     store += str[i]                                // Concatenate each character
+// }
+// console.log(store);
+
+// count till 1 to 10
+
+// var sum = 0
+// for (i=0 ; i<=10 ; i++){
+//     sum = sum+i
+//     console.log(sum);
+// }
+
+//While loop : entry level condition checking loop
+
+//syntax :
+
+// let count = 0  //=> initiallzation
+// while (count<5) {   //=> condition
+//     console.log(count);   // =. printing statement
+//     count++ // => iteration
+// }
+
+// Do While loop :
+ 
+// let docount = 0
+// do{
+//     console.log(docount);
+//     docount++
+// }while(docount<5)
+
+
